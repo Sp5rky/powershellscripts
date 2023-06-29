df789f798432aa566e95704a1ba202b1b89099a7
Sp5rky <29722511+Sp5rky@users.noreply.github.com>
2023-06-29 13:28:39 -0600

    🔧 chore(RenamePCCartesian.ps1): refactor script to remove unnecessary line break and improve code readability

--- New Device/Cartestian/RenamePCCartesian.ps1
+++ New Device/Cartestian/RenamePCCartesian.ps1
@@ -1,3 +1,2 @@
 # Get the PCs serial number and rename the PC to cart-<serial number>
-$Serial = (Get-WmiObject Win32_BIOS).SerialNumber
-Rename-Computer -NewName cart-$Serial -Force
+$Serial = (Get-WmiObject Win32_BIOS).SerialNumber; Rename-Computer -NewName cart-$Serial -Force
